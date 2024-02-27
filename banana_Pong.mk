d8139af20960a52838bc10f3e685fa598038b350
root <root@banana.com>
2024-02-28 05:20:52 +0530

    initial banana push for official

--- banana_Pong.mk
+++ banana_Pong.mk
@@ -4,21 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from those products - Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from Pong device
 $(call inherit-product, device/nothing/Pong/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := lineage_Pong
+# Device identifier
+PRODUCT_NAME := banana_Pong
 PRODUCT_DEVICE := Pong
 PRODUCT_MANUFACTURER := nothing
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A065
+
+# BananaDroid
+BANANA_MAINTAINER := Nobody
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDE_INNERTUNE := true
+TARGET_EXCLUDE_MATLOG := true
+
+# Gapps
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
 
 PRODUCT_SYSTEM_NAME := Pong
 PRODUCT_SYSTEM_DEVICE := Pong
